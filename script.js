519a51cf4a48a238979c77f3e3845bbe8ffc39f7
Yellam Pradeep <pradeepyellam@gmail.com>
2025-12-09 14:12:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const text = ["Full Stack Developer", "Python Developer", "Web Developer"];
 let index = 0;
 let charIndex = 0;
+
 const typingElement = document.querySelector(".typing");
-const toggle = document.getElementById("theme-toggle");
-const body = document.body;
 
 function type() {
     if (charIndex < text[index].length) {
@@ -34,20 +33,4 @@ window.addEventListener("scroll", () => {
             el.classList.add("active");
         }
     });
-});
-
-if (localStorage.getItem("theme") === "dark") {
-    body.classList.add("dark-mode");
-    toggle.textContent = "☀️";
-}
-
-toggle.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-    if (body.classList.contains("dark-mode")) {
-        toggle.textContent = "☀️";
-        localStorage.setItem("theme", "dark");
-    } else {
-        toggle.textContent = "🌙";
-        localStorage.setItem("theme", "light");
-    }
 });
